c14cd020e20f828b79562fc8cbb2a0fe23db72f5
= <=>
2016-06-04 14:53:37 -0500

    pract4

---
+++ practica4/ejercicio4/javascripts/app.js
@@ -0,0 +1,10 @@
+var main = function(){
+var url ="http://api.flickr.com/services/feeds/photos_public.gne?"+"tags=paisajes&format=json";
+/*	var url="test.json"
+	$.getJSON(url,function(respuesta){
+		console.log(respuesta);
+	}
+	);
+	//console.log("practica");*/
+}
+$(document).ready(main);
